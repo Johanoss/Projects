31b711b1949e199f645025165246a9ca1cc8bdcc
Johanoss <jonathanmarkagbulos@gmail.com>
2026-01-13 13:24:16 +0800

    Add files via upload

---
+++ test/firstsfc/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ test/firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Kailan natutong lumipad ang mga ibon?',
+      message: 'Since bird'
+    }
+  }
+};
+</script>
+
+<style>
+ #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+  #app > div:hover {
+  cursor: pointer;
+}
+</style>
